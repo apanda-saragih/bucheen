1450fda9a2756216ecdcd1364eab57fd27427b00
Abraham Putra Lukas <aam@10-60-32-94.0100.bsd.id.iosda.org>
2023-04-14 14:00:07 +0700

    INIT

---
+++ Bucheen/BucheenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucheenApp.swift
+//  Bucheen
+//
+//  Created by Abraham Putra Lukas on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BucheenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
